30e28df661a642086bc9cb5fd6f85d3c4f75e1e9
Ryan <ryanbent01@gmail.com>
2018-06-20 23:43:51 -0500

    Add tilt library and background color for logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 
 
 class App extends Component {
@@ -8,8 +9,8 @@ class App extends Component {
     return (
       <div className="App">
         <Navigation />
-        {/*<Logo />
-        <ImageLinkForm />
+        <Logo />
+        {/*<ImageLinkForm />
         <FaceRecognition />*/}
       </div>
     );
